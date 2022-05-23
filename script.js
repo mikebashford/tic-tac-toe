eb02b78250d7ed17eb717b74fbace5bf5ba52489
Michael Bashford <mikebashford@gmail.com>
2022-05-22 19:56:35 -0500

    Added board

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-class TicTacToeGame {}
+class TicTacToeGame {
+  drawBoard() {
+    document.body.innerHTML = "";
+    let gameBoard = document.createElement("div");
+    gameBoard.id = "gameBoard";
+    gameBoard.classList.add("board");
+
+    this.board.forEach((square, index) => {
+      let squareElement = document.createElement("div");
+      squareElement.id = index;
+      squareElement.classList.add("square");
+      gameBoard.appendChild(squareElement);
+    });
+
+    document.body.appendChild(gameBoard);
+  }
+
+  start() {
+    this.board = ["", "", "", "", "", "", "", "", ""];
+    this.drawBoard();
+  }
+}
+
+const game = new TicTacToeGame();
+game.start();

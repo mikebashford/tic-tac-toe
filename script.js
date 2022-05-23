8612b1dc37f773f60258eaba2c087177ae9944ee
Michael Bashford <mikebashford@gmail.com>
2022-05-22 20:44:39 -0500

    Added win checking conditions

--- script.js
+++ script.js
@@ -9,13 +9,17 @@ class TicTacToeGame {
   executeMove(moveIndex) {
     if (this.board[moveIndex] == "") {
       this.board[moveIndex] = this.currentPlayer;
-      this.currentPlayer =
-        this.currentPlayer == PLAYER_ONE_SYMBOL
-          ? PLAYER_TWO_SYMBOL
-          : PLAYER_ONE_SYMBOL;
+      this.updateBoard();
+      if (!this.checkWinner()) {
+        this.currentPlayer =
+          this.currentPlayer == PLAYER_ONE_SYMBOL
+            ? PLAYER_TWO_SYMBOL
+            : PLAYER_ONE_SYMBOL;
+      } else {
+        alert("Player " + this.currentPlayer + " has won!");
+        this.start();
+      }
     }
-    this.updateBoard();
-    console.log(this.board);
   }
 
   updateBoard() {
@@ -24,6 +28,32 @@ class TicTacToeGame {
     squareElements.forEach((element, index) => {
       if (element.innerText != this.board[index]) {
         element.innerText = this.board[index];
+      }
+    });
+  }
+
+  checkWinner() {
+    const winningCombos = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    return winningCombos.find((combo) => {
+      if (
+        this.board[combo[0]] != "" &&
+        this.board[combo[1]] != "" &&
+        this.board[combo[2]] != "" &&
+        this.board[combo[0]] == this.board[combo[1]] &&
+        this.board[combo[1]] == this.board[combo[2]]
+      ) {
+        return true;
+      } else {
+        return false;
       }
     });
   }
